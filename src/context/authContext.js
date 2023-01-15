7d2b2355fc6309af58200c80038fb074d931bbff
kanhaiya <kanhaiya2778@gmail.com>
2023-01-15 16:33:17 +0530

    fix auth state issue

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -5,18 +5,21 @@ const authContext=React.createContext({isAuthenticated:false})
 export const useAuthContext = ()=>useContext(authContext);
 
 export const AuthContextProvider = ({children})=>{
-    const [isAuthenticated,setIsAuthenticated] = useState(false)
     const [isAuthModalOpen,setIsAuthModalOpen] = useState(false)
+    const [isAuthenticated,setIsAuthenticated] = useState(false)
    useEffect(()=>{
-   
-        if (auth.currentUser) {
-          setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
+   auth.onAuthStateChanged((user)=>{
+    console.log("Auth state changed",user);
+    if (user) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
 
-        }
+    }
+   })
+       
       
-   },[auth.currentUser])
+   },[])
     const value = {isAuthenticated,setIsAuthenticated,isAuthModalOpen,setIsAuthModalOpen}
     return(
         <authContext.Provider value={value}>{children}</authContext.Provider>
