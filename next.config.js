c320015314e63b6ea4e66bdfe350abe6484eb51a
kanhaiya <kanhaiya2778@gmail.com>
2023-01-18 23:00:46 +0530

    edit next config file

--- next.config.js
+++ next.config.js
@@ -1,36 +1,36 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: false,
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'user-images.githubusercontent.com',
-//         port: '',
-//         pathname: '/**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'firebasestorage.googleapis.com',
-//         port: '',
-//         pathname: '/**',
-//       },
-//       {
-//         protocol: 'https',
-//         hostname: 'uploads-ssl.webflow.com',
-//         port: '',
-//         pathname: '/**',
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "**",
-//     }
-//     ],
-//   },
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'user-images.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'uploads-ssl.webflow.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+    }
+    ],
+  },
+}
 
-// module.exports = nextConfig
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: true,
-})
-module.exports = withBundleAnalyzer({})
+module.exports = nextConfig
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: true,
+// })
+// module.exports = withBundleAnalyzer({})
