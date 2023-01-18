92fa5f183d28dde6358940e97b8ced4194ca0474
kanhaiya <kanhaiya2778@gmail.com>
2023-01-18 13:05:55 +0530

    disable lazyload

--- src/components/TopSection.js
+++ src/components/TopSection.js
@@ -11,18 +11,25 @@ const slots = {
 const TopSection = ({ data }) => {
   const titleData = data.title;
   let titleEl;
-  if(titleData){
-    titleEl = titleData.map((txt,i)=>{
-      if(txt["gradient"]){
-        return <GradientText key={i}>{txt["gradient"]}</GradientText>
+  if (titleData) {
+    titleEl = titleData.map((txt, i) => {
+      if (txt["gradient"]) {
+        return <GradientText key={i}>{txt["gradient"]}</GradientText>;
       }
-      return <span key={i}>{txt["normal"]}</span>
-    })
+      return <span key={i}>{txt["normal"]}</span>;
+    });
   }
   return (
     <div className="bg-dark-primary-color w-full text-white relative overflow-hidden z-10 px-[5%] py-4">
       <div className="bg-dark-primary-color absolute top-0 left-0 w-full h-full flex items-start justify-start">
-        <Image src={data.bgImage} fill={true} style={{objectFit:"contain",objectPosition:"0% 0%"}} alt="background" className="w-[50%]" priority={true}/>
+        <Image
+          src={data.bgImage}
+          fill={true}
+          style={{ objectFit: "contain", objectPosition: "0% 0%" }}
+          alt="background"
+          className="w-[50%]"
+          priority={true}
+        />
       </div>
       <div className="pt-20 md:pt-32 flex flex-col items-center mx-auto max-w-[1100px] text-center relative">
         <h1 className="text-3xl md:text-5xl font-bold mb-4 md:px-[10%]">
@@ -33,12 +40,12 @@ const TopSection = ({ data }) => {
           {data.description}
         </h5>
         <div className="flex flex-col-reverse lg:flex-row w-full my-5 ">
-        <InfoCard data={data} />
+          <InfoCard data={data} />
           <VideoCard data={data} />
         </div>
-      <div className="bg-slate-700 mx-auto w-[80%] h-[1px]"></div>
+        <div className="bg-slate-700 mx-auto w-[80%] h-[1px]"></div>
 
-        <SlotPicker data={data}/>
+        <SlotPicker data={data} />
       </div>
     </div>
   );
@@ -49,24 +56,52 @@ const InfoCard = ({ data }) => {
     <div className="flex-1 mx-2 rounded-xl bg-dark-secondary-color p-5">
       <div className="grid grid-cols-2 grid-rows-2 w-full gap-2">
         <div className=" bg-dark-primary-color px-2 md:px-4 h-20 rounded-xl flex justify-start items-center">
-          <Image width={20} height={20} priority={true} src="/images/calender.svg" alt="" />
-          <span className="text-sm md:text-base font-semibold ml-3 text-left">30 Days</span>
+          <Image
+            width={20}
+            height={20}
+            priority={true}
+            src="/images/calender.svg"
+            alt=""
+          />
+          <span className="text-sm md:text-base font-semibold ml-3 text-left">
+            30 Days
+          </span>
         </div>
         <div className=" bg-dark-primary-color px-2 md:px-4 h-20 rounded-xl flex justify-start items-center">
-          <Image width={20} height={20} priority={true} src="/images/double-chevron-right.svg" alt="" />
+          <Image
+            width={20}
+            height={20}
+            priority={true}
+            src="/images/double-chevron-right.svg"
+            alt=""
+          />
           <span className="text-sm md:text-base font-semibold ml-3 text-left">
             Starts from {moment(data.startDate).format("Do MMMM,YYYY")}
           </span>
         </div>
         <div className=" bg-dark-primary-color px-2 md:px-4 h-20 rounded-xl flex justify-start items-center">
-          <Image width={20} height={20} priority={true} src="/images/video.svg" alt="" />
+          <Image
+            width={20}
+            height={20}
+            priority={true}
+            src="/images/video.svg"
+            alt=""
+          />
           <span className="text-sm md:text-base font-semibold ml-3 text-left">
             Virtual Class + QnA chat
           </span>
         </div>
         <div className=" bg-dark-primary-color px-2 md:px-4 h-20 rounded-xl flex justify-start items-center">
-          <Image width={20} height={20} priority={true} src="/images/hour-glass.svg" alt="users" />
-          <span className="text-sm md:text-base font-semibold ml-3 text-left">45 minutes/class</span>
+          <Image
+            width={20}
+            height={20}
+            priority={true}
+            src="/images/hour-glass.svg"
+            alt="users"
+          />
+          <span className="text-sm md:text-base font-semibold ml-3 text-left">
+            45 minutes/class
+          </span>
         </div>
       </div>
       {/* Intructor */}
@@ -74,9 +109,15 @@ const InfoCard = ({ data }) => {
         <h1 className="text-2xl text-slate-200 mb-4">Meet your instructor</h1>
         <div className="flex items-center justify-center  gap-4">
           <div class="max-w-[300px] w-full ">
-          <div className=" rounded-md overflow-hidden relative pt-[100%]">
-            <Image src={data.instructor.image} alt="Instructor" fill={true} style={{objectFit:"cover",objectPosition:"0% 0%"}}  priority={true}/>
-          </div>
+            <div className=" rounded-md overflow-hidden relative pt-[100%]">
+              <Image
+                src={data.instructor.image}
+                alt="Instructor"
+                fill={true}
+                style={{ objectFit: "cover", objectPosition: "0% 0%" }}
+                priority={true}
+              />
+            </div>
           </div>
           <div className="flex flex-col">
             <h3 className="text-2xl font-bold mb-1">{data.instructor.name}</h3>
@@ -88,13 +129,25 @@ const InfoCard = ({ data }) => {
       </div>
       <div className="grid grid-cols-2 w-full gap-2 mb-8">
         <div className=" bg-dark-primary-color px-2 md:px-4 h-20 rounded-xl flex justify-start items-center">
-          <Image width={20} height={20} priority={true} src="/images/planner.svg" alt="" />
+          <Image
+            width={20}
+            height={20}
+            priority={true}
+            src="/images/planner.svg"
+            alt=""
+          />
           <span className="text-sm md:text-base font-semibold ml-3 text-left">
             Personilzed Diet Plans
           </span>
         </div>
         <div className=" bg-dark-primary-color px-2 md:px-4 h-20 rounded-xl flex justify-start items-center">
-          <Image width={20} height={20} priority={true} src="/images/clock.svg" alt="" />
+          <Image
+            width={20}
+            height={20}
+            priority={true}
+            src="/images/clock.svg"
+            alt=""
+          />
           <span className="text-sm md:text-base font-semibold ml-3 text-left">
             Convinient Time Slots
           </span>
@@ -132,19 +185,25 @@ const VideoPlayer = ({ video }) => {
           </video>
         ) : (
           <div>
-            
             <Image
               src={video.thumbnail}
               alt="Introduction"
               className="opacity-50 w-full"
               fill={true}
-              style={{objectFit:"cover"}}
+              style={{ objectFit: "cover" }}
+              priority={true}
             />
             <button
               className="absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col items-center bg-slate-800 bg-opacity-50 p-2 rounded-lg"
               onClick={handleClick}
             >
-              <Image src="/images/play.svg" alt="Play" width={32} height={32}/>
+              <Image
+                src="/images/play.svg"
+                alt="Play"
+                width={32}
+                height={32}
+                priority={true}
+              />
               <span className="text-sm">Watch Trailer</span>
             </button>
           </div>
@@ -153,7 +212,7 @@ const VideoPlayer = ({ video }) => {
     </div>
   );
 };
-const SlotPicker = ({data}) => {
+const SlotPicker = ({ data }) => {
   const [selectedSlot, setSelectedSlot] = useState(null);
   return (
     <div className="flex flex-col items-start mt-8">
@@ -200,7 +259,10 @@ const SlotPicker = ({data}) => {
           })}
         </div>
       </div>
-      <EnrollButton applyClasses="py-4 mt-4 text-lg" disabled={!Boolean(selectedSlot) && true}>
+      <EnrollButton
+        applyClasses="py-4 mt-4 text-lg"
+        disabled={!Boolean(selectedSlot) && true}
+      >
         Book your slot at &#x20b9; {data.price}
       </EnrollButton>
     </div>
