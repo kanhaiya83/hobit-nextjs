ba318870cd597a7a14d941ed82be10fdb9520d6f
kanhaiya <kanhaiya2778@gmail.com>
2023-01-20 17:44:52 +0530

    change postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,12 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    "@fullhuman/postcss-purgecss":{}
+    '@fullhuman/postcss-purgecss':{
+                content: [
+                    './pages/**/*.{js,jsx,ts,tsx}',
+                    './components/**/*.{js,jsx,ts,tsx}'
+                ],
+                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+            }
   },
 }
