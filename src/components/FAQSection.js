780154ac355e16cb932dadce2e83b0b47c3718b1
kanhaiya <kanhaiya2778@gmail.com>
2023-01-24 19:24:33 +0530

    design changes  #4

--- src/components/FAQSection.js
+++ src/components/FAQSection.js
@@ -6,22 +6,22 @@ import SectionHeading from "./SectionHeading";
 const FAQSection = ({ data }) => {
   const List = [
     {
-      question: "1.Will I get to interact with my tutor?",
+      question: "Will I get to interact with my tutor?",
       answer:
         "Yes! All the doubts and queries would be addressed through built in chat option and WhatsApp portal would also be active where your tutor would respond you within 24 hours of addressing your query.",
     },
     {
-      question: "3.Do I need any equipment?",
+      question: "Do I need any equipment?",
       answer:
         "No! These are no equipment workouts for you to practice them easily at home.",
     },
     {
-      question: "4.What is the procedure after I purchase this course?",
+      question: "What is the procedure after I purchase this course?",
       answer:
         "Once you register for our sessions, your course will be visible to you in ‘My stuff’. You can select your desired time slot once each day and can attend your session.",
     },
     {
-      question: "5.What if I miss a session?",
+      question: "What if I miss a session?",
       answer:
         "In case you miss a session, don’t worry. We offer flexible time slots to our users. You can select your desired time slot each day.",
     },
@@ -63,12 +63,12 @@ const AccordionItem = ({ question, answer }) => {
   const [isActive, setIsActive] = useState(false);
   const collapsibleRef = useRef();
   return (
-    <div className="w-full flex flex-col border-b border-slate-200">
+    <div className="w-full flex flex-col bg-[#fafafa] rounded-lg mb-4">
       <div
-        className="flex items-center justify-between   py-5 cursor-pointer hover:bg-[#f3f3f3] rounded"
+        className="flex items-center justify-between   py-5 cursor-pointer"
         onClick={() => setIsActive(!isActive)}
       >
-        <h1 className="text-xl px-4">{question}</h1>
+        <h1 className="text-lg md:text-xl px-4 font-semibold text-slate-700">{question}</h1>
         <div className={`w-8 h-8 relative mr-[5%] transition-all ${isActive && "rotate-180"}`}>
             <Image src="/images/chevron-down.svg" alt="Chevron" fill={true} style={{objectFit:"contain"}}/>
         </div>
@@ -78,7 +78,7 @@ const AccordionItem = ({ question, answer }) => {
         style={{maxHeight:isActive?collapsibleRef?.current?.scrollHeight:"0px"}}
         ref={collapsibleRef}
       >
-        <h3 className="text-lg text-slate-700 p-4">{answer}</h3>
+        <h3 className="md:text-lg text-slate-700 p-4">{answer}</h3>
       </div>
     </div>
   );
