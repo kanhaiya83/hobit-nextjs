d95997ce2c2e017424994e2dd9ac552f23412547
kanhaiya <kanhaiya2778@gmail.com>
2023-01-31 15:26:44 +0530

    testing

--- src/context/razorpayContext.js
+++ src/context/razorpayContext.js
@@ -11,52 +11,52 @@ export const useRazorpayContext = () => useContext(razorpayContext);
 export const RazorpayContextProvider = ({ children,pageData }) => {
     const {setHasEnrolled,slot} = useAuthContext()
 
-  const Razorpay = useRazorpay();
-  const handlePayment = async (paymentData) => {
-    const {course,currency,amount} = paymentData;
-    if(!slot){
-        return warnToast("Please select a slot!")
-    }
-    try {
-      const response = await fetch("/api/razorpay", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ amount: amount, currency: currency }),
-      }).then((t) => t.json());
-      console.log(response);
-      var options = {
-        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY,
-        name: "Hobit",
-        currency: response.currency,
-        amount: response.amount,
-        order_id: response.id,
-        image: logoImage,
-        notes: {
-          course: course,
-          uid: auth.currentUser.uid,
-          mobile:auth.currentUser.phoneNumber,
-          slot:slot
-        },
-        handler: function (response) {
-          successToast("Payment successful!!");
-          setHasEnrolled(true);
-        },
-        prefill: {
-          contact: auth.currentUser.phoneNumber,
-        },
-        theme: {
-          color: "#8B53FF",
-        },
-      };
-      const rzpay = new Razorpay(options);
-    rzpay.open();
-    } catch (e) {
-      errorToast("Some error occurred!!");
-      console.log(e);
-    }
-  }
+  // const Razorpay = useRazorpay();
+  // const handlePayment = async (paymentData) => {
+  //   const {course,currency,amount} = paymentData;
+  //   if(!slot){
+  //       return warnToast("Please select a slot!")
+  //   }
+  //   try {
+  //     const response = await fetch("/api/razorpay", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ amount: amount, currency: currency }),
+  //     }).then((t) => t.json());
+  //     console.log(response);
+  //     var options = {
+  //       key: process.env.NEXT_PUBLIC_RAZORPAY_KEY,
+  //       name: "Hobit",
+  //       currency: response.currency,
+  //       amount: response.amount,
+  //       order_id: response.id,
+  //       image: logoImage,
+  //       notes: {
+  //         course: course,
+  //         uid: auth.currentUser.uid,
+  //         mobile:auth.currentUser.phoneNumber,
+  //         slot:slot
+  //       },
+  //       handler: function (response) {
+  //         successToast("Payment successful!!");
+  //         setHasEnrolled(true);
+  //       },
+  //       prefill: {
+  //         contact: auth.currentUser.phoneNumber,
+  //       },
+  //       theme: {
+  //         color: "#8B53FF",
+  //       },
+  //     };
+  //     const rzpay = new Razorpay(options);
+  //   rzpay.open();
+  //   } catch (e) {
+  //     errorToast("Some error occurred!!");
+  //     console.log(e);
+  //   }
+  // }
   return (
     <razorpayContext.Provider value={{handlePayment,pageData}}>
       {children}
