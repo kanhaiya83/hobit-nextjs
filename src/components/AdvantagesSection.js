6967d6fa64c18e6967c47deeb1d6f4fc878103dc
kanhaiya <kanhaiya2778@gmail.com>
2023-02-01 13:23:54 +0530

    change layout

--- src/components/AdvantagesSection.js
+++ src/components/AdvantagesSection.js
@@ -5,14 +5,14 @@ import SectionHeading from './SectionHeading'
 
 const AdvantagesSection = ({data}) => {
   return (
-    <div className="my-10 mx-auto px-[5%] lg:px-4 flex flex-col items-center" >
+    <div className="my-10 mx-auto px-[5%] lg:px-4 flex flex-col items-center max-w-[850px]" >
         <SectionHeading><GradientText>Why you should join this class?</GradientText></SectionHeading>
         <ul>
             {data.advantages.map((adv)=>{
                 return <li key={adv.id}>
                     <div className="flex items-center mb-3">
-                        <div className="mr-2 relative w-5 h-5 md:w-8 md:h-8">
-                        <Image src="/images/checked-circle.svg" alt="" fill={true} style={{objectFit:"fill"}}/>
+                        <div className="mr-2 relative w-5 h-5 md:w-8 md:h-8 flex-shrink-0">
+                        <Image src="/images/checked-circle.svg" alt="" fill={true} style={{objectFit:"contain"}}/>
                         </div>
                         <h4 className='text-sm md:text-2xl font-medium md:font-normal'>{adv.text}</h4>
                     </div>
