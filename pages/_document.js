9b9e780667dee12760d0c0f4d1c4ab94b3d45f21
Mister-H <hs913271@gmail.com>
2023-02-21 13:53:45 +0530

    fixed meta pixel code error

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
           <script async src={'/static/facebook_pixel.js'} />
+          <noscript><img height="1" width="1" style="display:none"
+                         src="https://www.facebook.com/tr?id=724029262513851&ev=PageView&noscript=1"
+          /></noscript>
       </Head>
       <body>
         <Main />
--- public/static/facebook_pixel.js
+++ public/static/facebook_pixel.js
@@ -1,7 +1,4 @@
-<!-- Meta Pixel Code -->
-
-<script>
-    !function(f,b,e,v,n,t,s)
+ !function(f,b,e,v,n,t,s)
     {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
         n.callMethod.apply(n,arguments):n.queue.push(arguments)};
         if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -11,8 +8,3 @@
     'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '724029262513851');
     fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-               src="https://www.facebook.com/tr?id=724029262513851&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
