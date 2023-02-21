9e34ca4d019b5071eb6114746d5cce0905b0b4eb
Mister-H <hs913271@gmail.com>
2023-02-21 13:50:35 +0530

    facebook pixel code connected

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+          <script async src={'/static/facebook_pixel.js'} />
+      </Head>
       <body>
         <Main />
         <NextScript />
