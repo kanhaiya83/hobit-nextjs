27032216eeacb0690da77a909b568ba5faf6b384
Mister-H <hs913271@gmail.com>
2023-02-22 15:46:07 +0530

    Added different script for facebook pixel for both zumba and cooking

--- pages/_app.js
+++ pages/_app.js
@@ -1,57 +1,83 @@
 import "../styles/globals.css";
 import "../styles/index.css";
 import "../styles/firebase.css";
-import { AuthContextProvider } from "../src/context/authContext";
+import {AuthContextProvider} from "../src/context/authContext";
 import Head from 'next/head'
 import localFont from '@next/font/local'
-
-import { ToastContainer } from 'react-toastify';
+import Script from 'next/script'
+import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import LoginModal from "../src/components/LoginModal";
-const gilroy = localFont({ src: [{
-  path: '../public/fonts/gilroy/Gilroy-Light.woff',
-  weight: '300',
-  style: 'normal',
-},{
-  path: '../public/fonts/gilroy/Gilroy-Regular.woff',
-  weight: '400',
-  style: 'normal',
-},{
-  path: '../public/fonts/gilroy/Gilroy-Medium.woff',
-  weight: '500',
-  style: 'normal',
-},{
-  path: '../public/fonts/gilroy/Gilroy-SemiBold.woff',
-  weight: '600',
-  style: 'normal',
-},{
-  path: '../public/fonts/gilroy/Gilroy-Bold.woff',
-  weight: '800',
-  style: 'normal',
-},{
-  path: '../public/fonts/gilroy/Gilroy-Heavy.woff',
-  weight: '900',
-  style: 'normal',
-}] ,variable: '--font-gilroy'})
+import {useRouter} from "next/router";
+import {useEffect, useState} from "react";
+
+const gilroy = localFont({
+    src: [{
+        path: '../public/fonts/gilroy/Gilroy-Light.woff',
+        weight: '300',
+        style: 'normal',
+    }, {
+        path: '../public/fonts/gilroy/Gilroy-Regular.woff',
+        weight: '400',
+        style: 'normal',
+    }, {
+        path: '../public/fonts/gilroy/Gilroy-Medium.woff',
+        weight: '500',
+        style: 'normal',
+    }, {
+        path: '../public/fonts/gilroy/Gilroy-SemiBold.woff',
+        weight: '600',
+        style: 'normal',
+    }, {
+        path: '../public/fonts/gilroy/Gilroy-Bold.woff',
+        weight: '800',
+        style: 'normal',
+    }, {
+        path: '../public/fonts/gilroy/Gilroy-Heavy.woff',
+        weight: '900',
+        style: 'normal',
+    }], variable: '--font-gilroy'
+})
 
 
-export default function App({ Component, pageProps }) {
+export default function App({Component, pageProps}) {
+    const router = useRouter();
+    const [pixelId, setPixelId] = useState(null);
+    const [scriptSrc, setScriptSrc] = useState(null);
 
-  return (
-    <>
-    <Head>
-    <title>Hobit</title>
-        <link rel="shortcut icon" href="/images/logo.png" />
-    </Head>
-      <AuthContextProvider>
-          <main className={`${gilroy.variable} font-sans`}>
+    useEffect(() => {
+        switch (router.pathname) {
+            case '/zumba':
+                setPixelId('724029262513851');
+                setScriptSrc('/static/facebook_pixel/zumba.js');
+                break;
+            case '/cooking':
+                setPixelId('488470343303714');
+                setScriptSrc('/static/facebook_pixel/cooking.js');
+                break;
+        }
 
-          <Component {...pageProps} />
-      <LoginModal/>
+    }, [router]);
 
-          </main>
- </AuthContextProvider>
-      <ToastContainer/>
-    </>
-  );
+    return (
+        <>
+            <Script async src={scriptSrc}/>
+            <Head>
+                <title>Hobit</title>
+                <link rel="shortcut icon" href="/images/logo.png"/>
+                <noscript><img height="1" width="1" style={{display: 'none'}}
+                               src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+                /></noscript>
+            </Head>
+            <AuthContextProvider>
+                <main className={`${gilroy.variable} font-sans`}>
+
+                    <Component {...pageProps} />
+                    <LoginModal/>
+
+                </main>
+            </AuthContextProvider>
+            <ToastContainer/>
+        </>
+    );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,18 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Head, Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-          <script async src={'/static/facebook_pixel.js'} />
-          <noscript><img height="1" width="1" style={{display: 'none'}}
-                         src="https://www.facebook.com/tr?id=724029262513851&ev=PageView&noscript=1"
-          /></noscript>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+
+    return (
+        <Html lang="en">
+            <Head>
+
+            </Head>
+            <body>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
 }
