2534b0da763de8cfd51ae3f5c86e4b595a88d8f2
Mister-H <hs913271@gmail.com>
2023-02-23 15:12:18 +0530

    testing react-facebook-pixel

--- pages/_app.js
+++ pages/_app.js
@@ -62,7 +62,7 @@ export default function App({Component, pageProps}) {
         });
 
 
-    }, [router]);
+    }, [pixelId, router]);
 
     return (
         <>
--- src/components/EnrollButton.js
+++ src/components/EnrollButton.js
@@ -1,13 +1,14 @@
 import {twMerge} from "tailwind-merge";
 import {useAuthContext} from "../context/authContext";
 import {useRazorpayContext} from "../context/razorpayContext";
-import ReactPixel from "react-facebook-pixel";
 
 const EnrollButton = ({children, onClick, disabled, applyClasses}) => {
     const {handlePayment} = useRazorpayContext();
     const {isAuthenticated, setIsAuthModalOpen, hasEnrolled} = useAuthContext();
     const handleClick = () => {
-        ReactPixel.track("InitiateCheckout", {currency: "INR", value: 999.00});
+        import('react-facebook-pixel').then((ReactPixel) => {
+            ReactPixel.default.track("InitiateCheckout", {currency: "INR", value: 999.00});
+        });
         if (!isAuthenticated) {
             return setIsAuthModalOpen(true);
         }
