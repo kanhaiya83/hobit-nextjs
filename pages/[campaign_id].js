2fb6b023a0074ca86a90e456e26ba1646ba2d1a1
Mister-H <hs913271@gmail.com>
2023-02-27 15:40:10 +0530

    added fb pixel code for cooking

--- pages/[campaign_id].js
+++ pages/[campaign_id].js
@@ -1,7 +1,7 @@
 import TopSection from "../src/components/TopSection";
 import FAQSection from "../src/components/FAQSection";
 import CheckboxSection from "../src/components/CheckboxSection";
-import { useEffect } from "react";
+import {useEffect, useState} from "react";
 import Header from "../src/components/Header";
 import TimerSection from "../src/components/TimerSection";
 import pagesData from "../src/constants/pages.json";
@@ -18,7 +18,26 @@ import { useRouter } from "next/router";
 import { RazorpayContextProvider } from "../src/context/razorpayContext";
 import Divider from "../src/components/Divider";
 import ChatButton from "../src/components/ChatButton";
+import Script from "next/script";
+import Head from "next/head";
 export default function CampaignPage({ data }) {
+  let html = ``;
+  let pixelId = null;
+  if(data.activity_name === 'Cooking') {
+    html = `
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1319617838557771');
+    fbq('track', 'PageView');
+    `;
+    pixelId = '1319617838557771';
+  }
   const { isAuthenticated, hasEnrolled, setHasEnrolled } = useAuthContext();
   const router = useRouter();
   const pid = router.query;
@@ -37,6 +56,18 @@ export default function CampaignPage({ data }) {
   }, [isAuthenticated]);
   return (
     <>
+      <Script
+        id={"facebook-pixel"}
+        strategy={"afterInteractive"}
+        dangerouslySetInnerHTML={{
+          __html:html
+        }}
+        />
+      <Head>
+        <noscript><img height="1" width="1" style={{display: 'none'}}
+                       src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+        /></noscript>
+      </Head>
       <RazorpayContextProvider pageData={data}>
         <div id="recaptcha-container" style={{ width: "100%" }}></div>
         <Header />
@@ -79,7 +110,7 @@ export async function getStaticProps(context) {
   } else {
     console.log("No data available");
   }
-  
+
 }
 export async function getStaticPaths() {
   return {
