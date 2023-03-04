ecef289d5a168fe36c0e3c9d8e01a4174cf9b83e
himanshu <hs913271@gmail.com>
2023-03-04 14:06:35 +0530

    applied heatmap js code in cooking page

--- pages/[campaign_id].js
+++ pages/[campaign_id].js
@@ -35,6 +35,11 @@ export default function CampaignPage({ data }) {
     'https://connect.facebook.net/en_US/fbevents.js');
     fbq('init', '1319617838557771');
     fbq('track', 'PageView');
+    
+    (function(h,e,a,t,m,p) {
+    m=e.createElement(a);m.async=!0;m.src=t;
+    p=e.getElementsByTagName(a)[0];p.parentNode.insertBefore(m,p);
+    })(window,document,'script','https://u.heatmap.it/log.js');
     `;
     pixelId = '1319617838557771';
   }
