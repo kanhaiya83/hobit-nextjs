4c6c4b0556ec79c52409dcb1bcca1fdc6ba75337
Mister-H <hs913271@gmail.com>
2023-03-06 14:33:41 +0530

    pixel coded added for all pages and clarity heatmap too

--- pages/[campaign_id].js
+++ pages/[campaign_id].js
@@ -1,146 +1,235 @@
 import TopSection from "../src/components/TopSection";
 import FAQSection from "../src/components/FAQSection";
 import CheckboxSection from "../src/components/CheckboxSection";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import Header from "../src/components/Header";
 import TimerSection from "../src/components/TimerSection";
-import pagesData from "../src/constants/pages.json";
 import Footer from "../src/components/Footer";
 import FeaturedSection from "../src/components/FeaturedSection";
 import SlidingTestimonials from "../src/components/SlidingTestimonials";
 import MentorSection from "../src/components/MentorSection";
 import AdvantagesSection from "../src/components/AdvantagesSection";
 import VideoTestimonialSection from "../src/components/VideoTestimonialSection";
-import { useAuthContext } from "../src/context/authContext";
-import { auth, db } from "../src/utils/firebase";
-import { ref, child, get } from "firebase/database";
-import { useRouter } from "next/router";
-import { RazorpayContextProvider } from "../src/context/razorpayContext";
+import {useAuthContext} from "../src/context/authContext";
+import {auth, db} from "../src/utils/firebase";
+import {child, get, ref} from "firebase/database";
+import {useRouter} from "next/router";
+import {RazorpayContextProvider} from "../src/context/razorpayContext";
 import Divider from "../src/components/Divider";
 import ChatButton from "../src/components/ChatButton";
 import Script from "next/script";
 import Head from "next/head";
-export default function CampaignPage({ data }) {
-  let html = ``;
-  let pixelId = null;
-  if(data.activity_name === 'Cooking') {
-    html = `
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '1319617838557771');
-    fbq('track', 'PageView');
-    
-    (function(h,e,a,t,m,p) {
-    m=e.createElement(a);m.async=!0;m.src=t;
-    p=e.getElementsByTagName(a)[0];p.parentNode.insertBefore(m,p);
-    })(window,document,'script','https://u.heatmap.it/log.js');
-    `;
-    pixelId = '1319617838557771';
-  }
-  const { isAuthenticated, hasEnrolled, setHasEnrolled } = useAuthContext();
-  const router = useRouter();
-  const pid = router.query;
-  useEffect(() => {
-    return
-    if (!isAuthenticated || hasEnrolled === pid) return;
-    (async () => {
-      const res = await fetch(
-        `/api/razorpay?uid=${auth.currentUser.uid}&course=${pid}`
-      );
-      const response = await res.json();
-      if (response.success) {
-        setHasEnrolled(pid);
-      }
-    })();
-  }, [hasEnrolled, isAuthenticated, pid, setHasEnrolled]);
-  return (
-    <>
-      <Script
-        id={"facebook-pixel"}
-        strategy={"afterInteractive"}
-        dangerouslySetInnerHTML={{
-          __html:html
-        }}
-        />
 
-      {/*Google Tag implementation*/}
-      <Script async={true} src="https://www.googletagmanager.com/gtag/js?id=AW-11109113326" />
-        <Script
-            id={"google-tag"}
-            strategy={"afterInteractive"}
-            dangerouslySetInnerHTML={{
-                __html:`window.dataLayer = window.dataLayer || [];
+export default function CampaignPage({data}) {
+    let html = ``;
+    let pixelId = null;
+    switch (data.activity_name) {
+        case 'Cooking':
+            html = `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1319617838557771');
+            fbq('track', 'PageView');
+            
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "g4aonndzcc");
+            `;
+            pixelId = '1319617838557771';
+            break;
+        case 'Zumba':
+            html = `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '488470343303714');
+            fbq('track', 'PageView');
+            
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "g4anuod2eu");
+            `;
+            pixelId = '488470343303714';
+            break;
+        case 'bhangra Fitness':
+            html = `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '72688042548112');
+            fbq('track', 'PageView');
+            
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "g4apbw7ex6");
+            `;
+            pixelId = '72688042548112';
+            break;
+        case 'Belly Dance':
+            html = `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '206049795340000');
+            fbq('track', 'PageView');
+            
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "g4aq6jmx2k");
+            `;
+            pixelId = '206049795340000';
+            break;
+        case 'yoga':
+            html = `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '73215676828834');
+            fbq('track', 'PageView');
+            
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "g4aqvkkhjb");
+            `;
+            pixelId = '73215676828834';
+            break;
+    }
+    const {isAuthenticated, hasEnrolled, setHasEnrolled} = useAuthContext();
+    const router = useRouter();
+    const pid = router.query;
+    useEffect(() => {
+        return
+        if (!isAuthenticated || hasEnrolled === pid) return;
+        (async () => {
+            const res = await fetch(
+                `/api/razorpay?uid=${auth.currentUser.uid}&course=${pid}`
+            );
+            const response = await res.json();
+            if (response.success) {
+                setHasEnrolled(pid);
+            }
+        })();
+    }, [hasEnrolled, isAuthenticated, pid, setHasEnrolled]);
+    return (
+        <>
+            <Script
+                id={"facebook-pixel"}
+                strategy={"afterInteractive"}
+                dangerouslySetInnerHTML={{
+                    __html: html
+                }}
+            />
+
+            {/*Google Tag implementation*/}
+            <Script async={true} src="https://www.googletagmanager.com/gtag/js?id=AW-11109113326"/>
+            <Script
+                id={"google-tag"}
+                strategy={"afterInteractive"}
+                dangerouslySetInnerHTML={{
+                    __html: `window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
                       
                         gtag('config', 'AW-11109113326');`
-            }}
-        />
+                }}
+            />
 
-      <Head>
-        <noscript><img height="1" width="1" style={{display: 'none'}}
-                       src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
-        /></noscript>
-      </Head>
-      <RazorpayContextProvider pageData={data}>
-        <div id="recaptcha-container" style={{ width: "100%" }}></div>
-        <Header />
-        <TopSection data={data} />
-        <FeaturedSection />
-        <Divider maxW={700} mx="auto" />
-        <VideoTestimonialSection data={data} />
-        <Divider maxW={700} mx="auto" />
-        <AdvantagesSection data={data} />
-        <Divider maxW={700} mx="auto" />
-        <SlidingTestimonials data={data} />
-        <Divider maxW={700} mx="auto" />
-        <TimerSection data={data} />
-        <Divider maxW={700} mx="auto" />
-        <CheckboxSection data={data} />
-        <Divider maxW={700} mx="auto" />
-        <MentorSection data={data} />
-        <Divider maxW={700} mx="auto" />
-        <FAQSection data={data} />
-        <Footer />
-      </RazorpayContextProvider>
-      <ChatButton />
-    </>
-  );
+            <Head>
+                <noscript><img height="1" width="1" style={{display: 'none'}}
+                               src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+                /></noscript>
+            </Head>
+            <RazorpayContextProvider pageData={data}>
+                <div id="recaptcha-container" style={{width: "100%"}}></div>
+                <Header/>
+                <TopSection data={data}/>
+                <FeaturedSection/>
+                <Divider maxW={700} mx="auto"/>
+                <VideoTestimonialSection data={data}/>
+                <Divider maxW={700} mx="auto"/>
+                <AdvantagesSection data={data}/>
+                <Divider maxW={700} mx="auto"/>
+                <SlidingTestimonials data={data}/>
+                <Divider maxW={700} mx="auto"/>
+                <TimerSection data={data}/>
+                <Divider maxW={700} mx="auto"/>
+                <CheckboxSection data={data}/>
+                <Divider maxW={700} mx="auto"/>
+                <MentorSection data={data}/>
+                <Divider maxW={700} mx="auto"/>
+                <FAQSection data={data}/>
+                <Footer/>
+            </RazorpayContextProvider>
+            <ChatButton/>
+        </>
+    );
 }
+
 export async function getStaticProps(context) {
-  const { params } = context;
-  const campaign_id = params.campaign_id;
-  const dbRef = ref(db);
-  const snapshot = await get(child(dbRef, `Campaigns`));
-  if (snapshot.exists()) {
-    const foundCampaign = snapshot.val()[campaign_id];
-    return {
-      props: {
-        data: foundCampaign,
-      },
-      revalidate: 30
+    const {params} = context;
+    const campaign_id = params.campaign_id;
+    const dbRef = ref(db);
+    const snapshot = await get(child(dbRef, `Campaigns`));
+    if (snapshot.exists()) {
+        const foundCampaign = snapshot.val()[campaign_id];
+        return {
+            props: {
+                data: foundCampaign,
+            },
+            revalidate: 30
 
-    };
-  } else {
-    console.log("No data available");
-  }
+        };
+    } else {
+        console.log("No data available");
+    }
 
 }
+
 export async function getStaticPaths() {
-  return {
-    paths: [
-      { params: { campaign_id: "zumba" } },
-      { params: { campaign_id: "cooking" } },
-      { params: { campaign_id: "bellydance" } },
-      { params: { campaign_id: "bhangra" } },
-      { params: { campaign_id: "yoga" } },
-    ],
-    fallback: false,
-  };
+    return {
+        paths: [
+            {params: {campaign_id: "zumba"}},
+            {params: {campaign_id: "cooking"}},
+            {params: {campaign_id: "bellydance"}},
+            {params: {campaign_id: "bhangra"}},
+            {params: {campaign_id: "yoga"}},
+        ],
+        fallback: false,
+    };
 }
--- src/context/razorpayContext.js
+++ src/context/razorpayContext.js
@@ -1,161 +1,164 @@
-import React, { useContext, useEffect } from "react";
+import React, {useContext, useEffect} from "react";
 import useRazorpay from "react-razorpay";
-import { useRouter } from "next/router";
-import { ref, child, get } from "firebase/database";
-import { auth, db } from "../utils/firebase";
-import { errorToast, successToast, warnToast } from "../utils/toast";
-import { useAuthContext } from "./authContext";
+import {useRouter} from "next/router";
+import {child, get, ref} from "firebase/database";
+import {auth, db} from "../utils/firebase";
+import {errorToast, successToast} from "../utils/toast";
+import {useAuthContext} from "./authContext";
 import axios from "axios";
-import { getProductUID, parseSlot } from "../utils";
+import {getProductUID, parseSlot} from "../utils";
 import fbq from "../utils/fbq";
 import gtag from "../utils/gtag";
+
 const http = axios.create({
-  baseURL: "https://asia-southeast1-hobitapp-22cb6.cloudfunctions.net/",
-  headers: {
-    Authorization:
-      "Bearer acd4cfd7157c9af0922acf9a826591a16655ed43697cb016d0effbe5954d02ba",
-  },
+    baseURL: "https://asia-southeast1-hobitapp-22cb6.cloudfunctions.net/",
+    headers: {
+        Authorization:
+            "Bearer acd4cfd7157c9af0922acf9a826591a16655ed43697cb016d0effbe5954d02ba",
+    },
 });
 const razorpayContext = React.createContext({
-  handlePayment: () => {},
-  pageData: {},
+    handlePayment: () => {
+    },
+    pageData: {},
 });
 
 export const useRazorpayContext = () => useContext(razorpayContext);
 
-export const RazorpayContextProvider = ({ children, pageData }) => {
-  const { user, slot,setHasEnrolled, isAuthenticated, setIsAuthModalOpen } = useAuthContext();
-  const router = useRouter();
-  const productUID = getProductUID(pageData.campaign_id, pageData.startDate);
-  const campaignId = pageData.campaign_id;
-  const amount = pageData.price;
-  const Razorpay = useRazorpay();
-  useEffect(() => {
-    if(!user)return;
-    const dbRef = ref(db);
-    get(child(dbRef, `MyLive/${user.uid}/${productUID}`))
-      .then((snapshot) => {
-        console.log("Checking if course already bought")
-        if (snapshot.exists()) {
-          console.log(snapshot.val());
-          setHasEnrolled(true)
-          successToast("You have already enrolled! Redirecting to main site!")
-           setTimeout(() => {
-                router.push("https://hobit.in/my-stuff");
-              }, 4000);
+export const RazorpayContextProvider = ({children, pageData}) => {
+    const {user, slot, setHasEnrolled, isAuthenticated, setIsAuthModalOpen} = useAuthContext();
+    const router = useRouter();
+    const productUID = getProductUID(pageData.campaign_id, pageData.startDate);
+    const campaignId = pageData.campaign_id;
+    const amount = pageData.price;
+    const Razorpay = useRazorpay();
+    useEffect(() => {
+        if (!user) return;
+        const dbRef = ref(db);
+        get(child(dbRef, `MyLive/${user.uid}/${productUID}`))
+            .then((snapshot) => {
+                console.log("Checking if course already bought")
+                if (snapshot.exists()) {
+                    console.log(snapshot.val());
+                    setHasEnrolled(true)
+                    successToast("You have already enrolled! Redirecting to main site!")
+                    setTimeout(() => {
+                        router.push("https://hobit.in/my-stuff");
+                    }, 4000);
 
-        } else {
-          console.log("No data available");
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, [productUID, router, setHasEnrolled, user]);
-  const handlePayment = async (paymentData) => {
-    if (!isAuthenticated) {
-      return setIsAuthModalOpen(true);
-    }
-    // if (!slot) {
-    //   document.getElementById("slot-picker")?.scrollIntoView();
-    //   return warnToast("Please select a slot!");
-    // }
-    if (!productUID || !campaignId || !amount) {
-      console.log("Error while initiating payment:", {
-        productUID,
-        campaignId,
-        amount,
-      });
-    }
-    try {
-      const createdOrder = await http.post(`createOrder`, {
-        UID: productUID,
-        userUID: user.uid,
-        type: "Live",
-        OS: "android",
-        campaign: {
-          isCampaignPurchase: true,
-          campaignId,
-        },
-      });
-      console.log("order creation response", createdOrder);
-      const order_id = createdOrder.data?.data?.id;
-      var options = {
-        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY,
-        name: "Hobit",
-        currency: "INR",
-        order_id,
-        image: "https://s3.amazonaws.com/rzp-mobile/images/rzp.png",
-        description: productUID,
-        handler: async function (response) {
-          const paymentDetails = {
-            customerId: user.uid,
-            productId: productUID,
-            type: "Live",
-            discountCode: "",
-            amount: amount,
-            paymentId: response.razorpay_payment_id,
-          };
-          try {
-            const paymentResponse = await http.post(`paymentRazorpay`, {
-              details: paymentDetails,
-              timeSlot: parseSlot(slot),
-            });
-            console.log("Order success response:", paymentResponse);
-            console.log(paymentResponse.data, paymentResponse.data.status);
-            if (paymentResponse.data?.status) {
-              successToast(
-                "Payment successful!Please login at hobit.in to access your course",
-                { autoClose: 4000 }
-              );
-                fbq("track", "Purchase", {
-                  currency: "INR",
-                    value: amount,
-                });
-                gtag('event', 'conversion', {
-                'send_to': 'AW-11109113326/Sg2ICISkx48YEO67nrEp',
-                'transaction_id': ''
-              });
-              router.push({
-                pathname: "/thankyou",
-                query: {
-                    productUID,
-                    amount,
-                    campaignId
+                } else {
+                    console.log("No data available");
                 }
-              });
-            } else {
-              errorToast("Some error occurred while making payment!!");
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [productUID, router, setHasEnrolled, user]);
+    const handlePayment = async (paymentData) => {
+        if (!isAuthenticated) {
+            return setIsAuthModalOpen(true);
+        }
+        // if (!slot) {
+        //   document.getElementById("slot-picker")?.scrollIntoView();
+        //   return warnToast("Please select a slot!");
+        // }
+        if (!productUID || !campaignId || !amount) {
+            console.log("Error while initiating payment:", {
+                productUID,
+                campaignId,
+                amount,
+            });
+        }
+        try {
+            const createdOrder = await http.post(`createOrder`, {
+                UID: productUID,
+                userUID: user.uid,
+                type: "Live",
+                OS: "android",
+                campaign: {
+                    isCampaignPurchase: true,
+                    campaignId,
+                },
+            });
+            console.log("order creation response", createdOrder);
+            const order_id = createdOrder.data?.data?.id;
+            var options = {
+                key: process.env.NEXT_PUBLIC_RAZORPAY_KEY,
+                name: "Hobit",
+                currency: "INR",
+                order_id,
+                image: "https://s3.amazonaws.com/rzp-mobile/images/rzp.png",
+                description: productUID,
+                handler: async function (response) {
+                    const paymentDetails = {
+                        customerId: user.uid,
+                        productId: productUID,
+                        type: "Live",
+                        discountCode: "",
+                        amount: amount,
+                        paymentId: response.razorpay_payment_id,
+                    };
+                    try {
+                        const paymentResponse = await http.post(`paymentRazorpay`, {
+                            details: paymentDetails,
+                            timeSlot: parseSlot(slot),
+                        });
+                        console.log("Order success response:", paymentResponse);
+                        console.log(paymentResponse.data, paymentResponse.data.status);
+                        if (paymentResponse.data?.status) {
+                            successToast(
+                                "Payment successful!Please login at hobit.in to access your course",
+                                {autoClose: 4000}
+                            );
+                            fbq("track", "Purchase", {
+                                currency: "INR",
+                                value: amount,
+                            });
+                            gtag('event', 'conversion', {
+                                'send_to': 'AW-11109113326/Sg2ICISkx48YEO67nrEp',
+                                'transaction_id': ''
+                            });
+                            router.push({
+                                pathname: "/thankyou",
+                                query: {
+                                    productUID,
+                                    amount,
+                                    campaignId
+                                }
+                            });
+                        } else {
+                            errorToast("Some error occurred while making payment!!");
+                        }
+                    } catch (e) {
+                        console.log("Error while payment", e);
+                    }
+                },
+                prefill: {
+                    contact: auth.currentUser.phoneNumber,
+                },
+                theme: {
+                    color: "#8B53FF",
+                },
+            };
+            const rzpay = new Razorpay(options);
+            rzpay.open();
+            // Validate
+            try {
+                const r = await http.post(`validateOrder`, {
+                    id: order_id,
+                    userUID: user.uid,
+                });
+                console.log("Order validation response:", r);
+            } catch (error) {
             }
-          } catch (e) {
-            console.log("Error while payment", e);
-          }
-        },
-        prefill: {
-          contact: auth.currentUser.phoneNumber,
-        },
-        theme: {
-          color: "#8B53FF",
-        },
-      };
-      const rzpay = new Razorpay(options);
-      rzpay.open();
-      // Validate
-      try {
-        const r = await http.post(`validateOrder`, {
-          id: order_id,
-          userUID: user.uid,
-        });
-        console.log("Order validation response:", r);
-      } catch (error) {}
-    } catch (e) {
-      errorToast("Some error occurred!!");
-      console.log(e);
-    }
-  };
-  return (
-    <razorpayContext.Provider value={{ handlePayment, pageData }}>
-      {children}
-    </razorpayContext.Provider>
-  );
+        } catch (e) {
+            errorToast("Some error occurred!!");
+            console.log(e);
+        }
+    };
+    return (
+        <razorpayContext.Provider value={{handlePayment, pageData}}>
+            {children}
+        </razorpayContext.Provider>
+    );
 };
