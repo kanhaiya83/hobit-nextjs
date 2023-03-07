4d900a914414b46bee419e91e37ece6e784958ae
Mister-H <hs913271@gmail.com>
2023-03-07 15:33:35 +0530

    facebook domain verification meta tag applied in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -73,6 +73,7 @@ export default function App({Component, pageProps}) {
                 {/*<noscript><img height="1" width="1" style={{display: 'none'}}*/}
                 {/*               src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}*/}
                 {/*/></noscript>*/}
+                <meta name="facebook-domain-verification" content="6oam35hs6nmocf7uqf86c8h5iesmka" />
             </Head>
             <AuthContextProvider>
                 <main className={`${gilroy.variable} font-sans`}>
