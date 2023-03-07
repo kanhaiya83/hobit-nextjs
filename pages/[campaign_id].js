2a989dc7ad11f133d8891cd25c3bc3107591ede0
Mister-H <hs913271@gmail.com>
2023-03-07 15:16:32 +0530

    domain verification meta tag inserted

--- pages/[campaign_id].js
+++ pages/[campaign_id].js
@@ -194,6 +194,7 @@ export default function CampaignPage({data}) {
                 <noscript><img height="1" width="1" style={{display: 'none'}}
                                src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
                 /></noscript>
+                <meta name="facebook-domain-verification" content="6oam35hs6nmocf7uqf86c8h5iesmka" />
             </Head>
             <RazorpayContextProvider pageData={data}>
                 <div id="recaptcha-container" style={{width: "100%"}}></div>
