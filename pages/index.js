663c53c8f75572be45f7620534c9dac9719f55d2
Mister-H <hs913271@gmail.com>
2023-03-07 17:36:37 +0530

    applied google tag on main domain

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,40 @@
 import Link from "next/link";
+import Script from "next/script";
 export default function Home() {
   return (
     <>
+        <Script async={true} src={"https://www.googletagmanager.com/gtag/js?id=AW-11109113326"}/>
+        <Script
+            id={'google-analytics'}
+            strategy={"afterInteractive"}
+            dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-11109113326');
+            `
+            }}
+        />
       <li>
-        
+
         <Link  className="underline text-blue-600 text-xl" href="/zumba">Zumba</Link>
       </li>
       <li>
-        
+
         <Link  className="underline text-blue-600 text-xl" href="/cooking">Cooking</Link>
       </li>
       <li>
-        
+
         <Link  className="underline text-blue-600 text-xl" href="/bellydance">Belly Dance</Link>
       </li>
       <li>
-        
+
         <Link  className="underline text-blue-600 text-xl" href="/bhangra">Bhangra Fitness</Link>
       </li>
       <li>
-        
+
         <Link  className="underline text-blue-600 text-xl" href="/yoga">Yoga</Link>
       </li>
     </>
